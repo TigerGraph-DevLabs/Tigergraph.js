0ddf91c691d2f7ae2e6f0fd5b00bce610ceab945
Saber Nomen <genericp3rson@gmail.com>
2020-09-07 23:53:49 -0500

    Added a callback for getToken

--- index.js
+++ index.js
@@ -4,22 +4,36 @@ let PASSWORD = "";
 let SECRET = "";
 let TOKEN = "";
 
-exports.getToken = (secret, domain = "localhost", lifetime = 1000000) => {
-  const https = require('https');
-
-  https.get(`https://${domain}.i.tgcloud.io:9000/requesttoken?secret=${secret}&lifetime=${lifetime}`, (resp) => {
+exports.getToken = (secret, domain = "localhost", lifetime = 1000000, callback = (ans) => { console.log(ans); }) => {
+  https.get(`https://${domain}.i.tgcloud.io:9000/requesttoken?secret=${secret}&lifetime=${lifetime}`, async (resp) => {
     let data = '';
     resp.on('data', (chunk) => {
       data += chunk;
     });
-    resp.on('end', () => {
-      console.log(JSON.parse(data)["token"]);
-    });
-
-  }).on("error", (err) => {
-    console.error(err);
+    resp.on('end', async () => {
+      return callback(JSON.parse(data)["token"])
+    })
   });
-} 
+}
+
+// exports.getToken = (secret, domain = "localhost", lifetime = 1000000) => {
+//   const https = require('https');
+
+//   https.get(`https://${domain}.i.tgcloud.io:9000/requesttoken?secret=${secret}&lifetime=${lifetime}`, (resp) => {
+//     let data = '';
+//     resp.on('data', (chunk) => {
+//       data += chunk;
+//     });
+//     resp.on('end', () => {
+//       let ans = await JSON.parse(data)["token"];
+//       console.log(ans);
+//       return ans;
+//     });
+
+//   }).on("error", (err) => {
+//     console.error(err);
+//   });
+// } 
 
 exports.createTigerGraphConnection = (domain, username, password, secret, token) => {
   DOMAIN = domain;
